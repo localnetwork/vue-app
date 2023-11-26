629b4b3e678ae78f445ebf5bb80c73518354e5b8
Diome Nike Potot <diome.halcyonwebdesign@gmail.com>
2023-11-26 19:51:01 +0800

    fixes await

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    supported: {
+      "top-level-await": true, //browsers can handle top-level-await features
+    },
+  },
+
   plugins: [vue()],
   // build: {
   //   envFiles: `.env.${process.env.NODE_ENV}`,
